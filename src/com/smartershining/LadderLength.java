d31eb33da2cbaa82ba075b4f349e33ff518da53b
smartershining <jxchen.swjtu@gmail.com>
2016-04-23 21:22:04 +0800

    add

---
+++ src/com/smartershining/LadderLength.java
@@ -0,0 +1,12 @@
+package com.smartershining;
+
+import java.util.Set;
+
+/**
+ * Created by smartershining on 16-4-23.
+ */
+public class LadderLength {
+    public int ladderLength(String beginWord, String endWord, Set<String> wordList) {
+      return 0;
+    }
+}
--- src/isAdditiveNumber.java
+++
@@ -1,9 +0,0 @@
-import java.math.BigInteger;
-
-/**
- * Created by smartershining on 16-3-26.
- */
-public class isAdditiveNumber {
-    
-}
-
